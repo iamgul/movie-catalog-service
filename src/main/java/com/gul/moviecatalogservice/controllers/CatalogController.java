5873afadb8dc1f06968ea98701d76b7f309c616c
Gul Mohammed <38118279+iamgul@users.noreply.github.com>
2024-01-29 22:25:33 +0545

    2nd Commit

--- src/main/java/com/gul/moviecatalogservice/controllers/CatalogController.java
+++ src/main/java/com/gul/moviecatalogservice/controllers/CatalogController.java
@@ -2,7 +2,7 @@ package com.gul.moviecatalogservice.controllers;
 
 import com.gul.moviecatalogservice.models.MovieCatalog;
 import com.gul.moviecatalogservice.models.MovieInfo;
-import com.gul.moviecatalogservice.models.Rating;
+import com.gul.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,16 +22,21 @@ public class CatalogController {
 
     @GetMapping("/{userId}")
     public List<MovieCatalog> getCatalog(@PathVariable String userId) {
-
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", "Rating 10"),
-                new Rating("5678", "Rating 9"));
-
-        return ratings.stream().map(rating -> {
+        UserRating response = restTemplate.getForObject("http://localhost:8082/ratings/" + userId, UserRating.class);
+        return response.getRatings().stream().map(rating -> {
             MovieInfo movieInfoResponse = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), MovieInfo.class);
             return new MovieCatalog(movieInfoResponse.getName(), movieInfoResponse.getDesc(), rating.getRating());
-        }).collect(Collectors.toList());
+        }).toList();
 
     }
 
+
 }
+/*
+MovieInfo movieInfoResponse =   webClientBuilder.build()
+        .get()
+        .uri("http://localhost:8081/movies/" + rating.getMovieId())
+        .retrieve()
+        .bodyToMono(MovieInfo.class)
+        .block();
+        */
